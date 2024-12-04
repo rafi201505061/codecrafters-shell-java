5ae9b2d73ba3c5865545767243e3d349200042cd
rafi201505061 <rakibul.rafi@revechat.com>
2024-12-04 08:35:37 +0600

    type command behaviour extended to support path

--- src/main/java/src/commands/TypeCommand.java
+++ src/main/java/src/commands/TypeCommand.java
@@ -1,6 +1,8 @@
 package src.commands;
 
+import java.io.File;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class TypeCommand extends Command {
   public TypeCommand(List<String> commandParts) {
@@ -12,14 +14,37 @@ public class TypeCommand extends Command {
     return commandParts.size() == 2 && commandParts.get(0).equals("type");
   }
 
-  @Override
-  public void execute() {
+  private boolean searchPath() {
+    String pathVariable = System.getenv("PATH");
+    StringTokenizer stringTokenizer = new StringTokenizer(pathVariable, ":");
+    while (stringTokenizer.hasMoreTokens()) {
+      File dir = new File(stringTokenizer.nextToken());
+      if (dir.isDirectory()) {
+        for (File file : dir.listFiles()) {
+          if (file.isFile() && file.canExecute() && file.getName().equals(commandParts.get(1))) {
+            System.out.println(file.getName() + " is " + file.getAbsolutePath());
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private void searchSupportedCommands() {
     try {
       CommandFactory factory = new ShellCommandFactory();
       Command command = factory.getCommand(commandParts.get(1));
       System.out.println(command.name + " is a shell builtin");
     } catch (Exception e) {
       System.out.println(e.getMessage());
+    }
+  }
+
+  @Override
+  public void execute() {
+    if (!searchPath()) {
+      searchSupportedCommands();
     }
   }
 
